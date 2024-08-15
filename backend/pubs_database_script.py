3f70e4533a85fe9fad35e223fa6bdeb75ae5f80a
Idil Yaktubay <idil.yaktubay@mail.utoronto.ca>
2024-08-15 10:19:19 -0700

    filter out unapproved records

--- backend/pubs_database_script.py
+++ backend/pubs_database_script.py
@@ -22,6 +22,9 @@ data = conn.read(worksheet="Publications")
 authors_data = conn.read(worksheet="Current_IISD-ELA_Authors")
 
 
+# filter out records that have not yet been approved by CH or IY
+data = data[data['approved']=='Yes' or data['approved']=='N/A']
+
 # Convert data types to string
     # This is so that years aren't displayed with decimals and
     # to avoid some data type errors
