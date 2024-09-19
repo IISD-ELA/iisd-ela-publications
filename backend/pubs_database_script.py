1205bd19b014a4587b8306e11eaa68a92f62cf64
Idil Yaktubay <idil.yaktubay@mail.utoronto.ca>
2024-09-19 15:20:21 -0700

    ...

--- backend/pubs_database_script.py
+++ backend/pubs_database_script.py
@@ -182,7 +182,7 @@ st.markdown(f"<h4 style='color: #083266;'>Notes</h4>", unsafe_allow_html=True)
 st.markdown('''**<div style="font-size: 12px;">
                     <u1> 
                         <li> The "Search by author(s)" function will only work for current IISD-ELA researchers.\nTo search by other researchers, please use the "General search" function. </li>
-                        <li> The "Search by" features of this search engine follow an "OR" logic, and the "Filter by" feature follows an "AND" logic. </li>
+                        <li> The "Search by" features of this search engine will generate results that match any of the tags you have selected. The "Filter by" feature will narrow down results to only those that meet the selected filter criteria. </li>
                     </u1>
             **</div>
             ''',
