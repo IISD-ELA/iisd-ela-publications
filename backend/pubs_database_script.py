7f6c37351c8fad3962887f768f58e837c1f933d4
Idil Yaktubay <idil.yaktubay@mail.utoronto.ca>
2024-09-19 14:55:48 -0700

    ...

--- backend/pubs_database_script.py
+++ backend/pubs_database_script.py
@@ -148,9 +148,9 @@ with col1:
     
     # Add a multi-select widget for lake tags
     # a. Get a list of all distinct (unique) lakes in the database
-    unique_lakes = list({int(num_str) for num_str in set(data['lake_tags'].str.split('; ').sum()) 
-                                if num_str.isdigit()})
-    unique_lakes.append('Other or Unspecified')
+    unique_lakes = {int(num_str) for num_str in set(data['lake_tags'].str.split('; ').sum()) 
+                                if num_str.isdigit()}
+    #unique_lakes.append('Other or Unspecified')
     
     # b. Add the lake tag widget
     lake_tags = st.multiselect(r"$\bold{Search} \: \bold{by} \: \bold{lake}$ ", 
