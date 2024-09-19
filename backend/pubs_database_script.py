f25529bfd8476ce2a570ed0aec3dd84ee7b1e6da
Idil Yaktubay <idil.yaktubay@mail.utoronto.ca>
2024-09-19 15:07:22 -0700

    ...

--- backend/pubs_database_script.py
+++ backend/pubs_database_script.py
@@ -178,7 +178,10 @@ with col1:
     # Add a general search box
     general_search_query = st.text_input(r"$\bold{General} \: \bold{search}$", "")
 
-st.markdown('**<div style="font-size: 12px;">Please note that the "Search by author(s)" function will only work for current IISD-ELA researchers.\nTo search by other researchers, please use the "General search" function.**</div>',
+st.markdown(f"<h4 style='color: #083266;'>Notes</h4>")
+st.markdown('**<div style="font-size: 12px;">* The "Search by author(s)" function will only work for current IISD-ELA researchers.\nTo search by other researchers, please use the "General search" function.**</div>',
+                unsafe_allow_html=True)
+st.markdown('**<div style="font-size: 12px;">* The "Search by" features of this search engine follow an "OR" logic, and the "Filter by" feature follows an "AND" logic.**</div>',
                 unsafe_allow_html=True)
 st.markdown('**<div style="font-size: 12px;">If you notice any missing publications or encounter issues with this search engine, please reach out to us at eladata@iisd-ela.org.**</div>',
                 unsafe_allow_html=True)
