dc744fe5bd014c378fa4a6b43a8c60202881a5d1
Idil Yaktubay <idil.yaktubay@mail.utoronto.ca>
2024-09-20 13:41:55 -0700

    ...

--- backend/pubs_database_script.py
+++ backend/pubs_database_script.py
@@ -228,9 +228,9 @@ with col2:
                 elif row['type']=='msc' or row['type']=='phd':
                     row_string = f"- {row['authors']} ({row['year']}). *{row['title']}* [{'Doctoral dissertation' if row['type']=='phd' else 'Master of Science dissertation'}, {row['thesis_uni']}]. {row['thesis_db']+'.' if not pd.isna(row['thesis_db']) else ''} {row['doi_or_url'] if not pd.isna(row['doi_or_url']) else ''}"
                 
-                tag_info = f''':blue[Lakes:] {row['lake_tags']}  
-                               :blue[Data Types:] {row['data_type_tags']}  
-                               :blue[Environmental Issues:] {row['environmental_issue_tags']}  
+                tag_info = f'''\033[1mLakes:\033[0m {row['lake_tags']}  
+                               \033[1mData Types:\033[0m {row['data_type_tags']}  
+                               \033[1mEnvironmental Issues:\033[0m {row['environmental_issue_tags']}  
                                '''
                 st.markdown(row_string, 
                             unsafe_allow_html=True,
