4edf70a21c4adf19cc2706e70accebc549dc27a5
Idil Yaktubay <idil.yaktubay@mail.utoronto.ca>
2024-09-20 11:53:33 -0700

    ...

--- backend/pubs_database_script.py
+++ backend/pubs_database_script.py
@@ -117,7 +117,7 @@ def combined_search(data,
               ]'''
     queries = [ 
                 (lake_query,
-                    data.apply(lambda row: print(row),
+                    data.apply(lambda row: row,
                                     #any(lake_tag in row['lake_tags'].split('; ')
                                      #   for lake_tag in lake_query),
                                 axis=1))
