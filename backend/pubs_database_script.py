0e30023990373e654f03a6b1e0dc4504964903c4
Idil Yaktubay <idil.yaktubay@mail.utoronto.ca>
2024-09-26 09:41:08 -0700

    cleaning up code...

--- backend/pubs_database_script.py
+++ backend/pubs_database_script.py
@@ -255,7 +255,9 @@ with col2:
             # Display each row as a string
             for index, row in result_for_user.iterrows():
                 if row['type'] == 'journal':
-                    row_string = f"- {row['authors']} ({row['year']}). {row['title']}. *{row['journal_name']}*, *{str(int(row['journal_vol_no']))}*({str(int(row['journal_issue_no']))}){', '+str(row['journal_page_range']) if not pd.isna(row['journal_page_range']) else ''}. {row['doi_or_url']}"
+                    row_string = f"- {row['authors']} ({row['year']}). \
+                                     {row['title']}. *{row['journal_name']}*, \
+                            *{str(int(row['journal_vol_no']))}*({str(int(row['journal_issue_no']))}){', '+str(row['journal_page_range']) if not pd.isna(row['journal_page_range']) else ''}. {row['doi_or_url']}"
                 elif row['type']=='msc' or row['type']=='phd':
                     row_string = f"- {row['authors']} ({row['year']}). *{row['title']}* [{'Doctoral dissertation' if row['type']=='phd' else 'Master of Science dissertation'}, {row['thesis_uni']}]. {row['thesis_db']+'.' if not pd.isna(row['thesis_db']) else ''} {row['doi_or_url'] if not pd.isna(row['doi_or_url']) else ''}"
                 
