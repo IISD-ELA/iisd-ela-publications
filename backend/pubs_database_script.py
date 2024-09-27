17a8636a4dc6a268dc8991f489a0fdebec84548e
Idil Yaktubay <idil.yaktubay@mail.utoronto.ca>
2024-09-27 11:17:14 -0700

    debugging...

--- backend/pubs_database_script.py
+++ backend/pubs_database_script.py
@@ -251,7 +251,7 @@ with col1:
     
     # Update initialized session states
     search_variables = [key for key in st.session_state.keys()]
-    f'''for i in range({len(search_variables)}):
+    f'''for i in range({search_variables}):
         input = inputs_list[i]
         variable = search_variables[i]
         st.session_state[input] = variable'''
