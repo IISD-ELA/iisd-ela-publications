5a1cf7ec0210a5237c5a0d520b9353273a2b1b55
Idil Yaktubay <idil.yaktubay@mail.utoronto.ca>
2024-10-02 12:30:30 -0700

    ...

--- backend/pubs_database_script.py
+++ backend/pubs_database_script.py
@@ -229,12 +229,14 @@ with col1:
                           function.
                        """
     if 'author_tags' in st.query_params:
-        author_tags = st.query_params.author_tags
-    author_tags = st.multiselect(r"$\bold{Search} \: \bold{by} \: \bold{authors}$",
-                                        options=sorted(iisd_ela_authors),
-                                        key='multi_author_tags',
-                                        help=tags_help_general + 
-                                            author_tags_help)
+        st.session_state.multi_author_tags = st.query_params.author_tags
+        author_tags = st.session_state.multi_author_tags
+    else:
+        author_tags = st.multiselect(r"$\bold{Search} \: \bold{by} \: \bold{authors}$",
+                                            options=sorted(iisd_ela_authors),
+                                            key='multi_author_tags',
+                                            help=tags_help_general + 
+                                                author_tags_help)
     
 
     # author_tags = multiselect_qs(r"$\bold{Search} \: \bold{by} \: \bold{authors}$",
