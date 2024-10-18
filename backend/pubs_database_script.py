38ebe752be3bb17d7652a68af791aac3d98a2d61
Idil Yaktubay <idil.yaktubay@mail.utoronto.ca>
2024-10-18 08:40:37 -0700

    change scientist list title

--- backend/pubs_database_script.py
+++ backend/pubs_database_script.py
@@ -294,7 +294,7 @@ if 'author_tags' in st.query_params:
 
 
     # Create title string for scientist
-    title_string_scientist = (f"<h2 style='color: #083266;'>Publications by "
+    title_string_scientist = (f"<h2 style='color: #083266;'>Academic Publications by "
                               f"{st.query_params.author_tags} "
                               f"({year_start_scientist}-{year_end_scientist})</h2>")
 
