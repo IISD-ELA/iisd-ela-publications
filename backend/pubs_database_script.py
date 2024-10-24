95d49a8f32a6e38e5f820a6c7459496f71d00642
Idil Yaktubay <idil.yaktubay@mail.utoronto.ca>
2024-10-24 13:52:28 -0700

    fix current year range

--- backend/pubs_database_script.py
+++ backend/pubs_database_script.py
@@ -450,7 +450,7 @@ result_for_user = combined_search(
 
 # Write disclaimer on bottom of page
 disclaimer_string = (f'**<div style="font-size: 18px;">Current year range: '
-                     f'{result_for_user["year"].min()}-{result_for_user["year"].max()}' 
+                     f'{data_year_min}-{data_year_max}' 
                      f'**</div>')
 
 st.markdown(disclaimer_string,
