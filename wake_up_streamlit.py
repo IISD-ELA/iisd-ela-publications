14bdd10661462f286626b2d543c35ad23fd740d9
idiyak <113523712+idiyak@users.noreply.github.com>
2025-09-25 14:04:20 -0700

    Update wake_up_streamlit.py

--- wake_up_streamlit.py
+++ wake_up_streamlit.py
@@ -79,7 +79,7 @@ from streamlit_app import STREAMLIT_APPS
 import datetime
 
 options = webdriver.ChromeOptions()
-options.add_argument('--headless=new')  # better JS support
+# options.add_argument('--headless=new')  # better JS support
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-gpu')
 
@@ -87,7 +87,9 @@ driver = webdriver.Chrome(options=options)
 
 with open("wakeup_log.txt", "a") as log_file:
     log_file.write(f"Execution started at: {datetime.datetime.now()}\n")
-
+    iframes = driver.find_elements(By.TAG_NAME, "iframe")
+    log_file.write(f"Found iframes: {len(iframes)}")
+            
     for url in STREAMLIT_APPS:
         try:
             driver.get(url)
@@ -100,7 +102,7 @@ with open("wakeup_log.txt", "a") as log_file:
             driver.save_screenshot("debug_screenshot.png")
             try:
                 # wait up to 15s for the button to appear
-                button = WebDriverWait(driver, 15).until(
+                button = WebDriverWait(driver, 30).until(
                     EC.element_to_be_clickable(
                         (By.XPATH, "//button[contains(text(), 'get this app back up')]")
                     )
